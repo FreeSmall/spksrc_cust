84bf3750693b3c25741d59a72db1f97daebfeca4
wrangler <pzhaoyang@126.com>
2024-11-06 09:54:26 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-podman image exists spksrc:cust || podman build -t spksrc:cust -f Dockerfile
-podman run -it --rm \
+docker images &>/dev/null && runner=docker
+podman images &>/dev/null && runner=podman
+
+[ -n "$runner" ] || { echo "env no docker or podman"; exit 0; }
+$runner image exists spksrc:cust || $runner build -t spksrc:cust -f Dockerfile
+$runner run -it --rm \
 --network host \
 -m="4g" \
 -v $(pwd):/spksrc \
